289d6b5cc09e9d88a66673f5cf913699216a9a00
AleXutzZu <davidalex_robert@yahoo.com>
2024-03-30 13:16:34 +0200

    Moved the structs to the database module

--- src-tauri/src/models.rs
+++ src-tauri/src/models.rs
@@ -1,24 +1,28 @@
-use diesel::prelude::*;
-use serde::Serialize;
 
-#[derive(Queryable, Selectable, Serialize)]
-#[diesel(table_name = crate::schema::users)]
-#[diesel(check_for_backend(diesel::sqlite::Sqlite))]
-#[serde(rename_all = "camelCase")]
-pub struct User {
-    pub id: i32,
-    pub username: String,
-    #[diesel(column_name = firstName)]
-    pub first_name: String,
-    #[diesel(column_name = lastName)]
-    pub last_name: String,
-}
+pub mod database {
+    use diesel::{Queryable, Selectable};
+    use serde::Serialize;
+    use diesel::prelude::*;
 
-#[derive(Queryable, Selectable)]
-#[diesel(table_name = crate::schema::books)]
-#[diesel(check_for_backend(diesel::sqlite::Sqlite))]
-pub struct Book {
-    pub id: i32,
-    pub title: String,
-    pub author: String,
+    #[derive(Queryable, Selectable, Serialize)]
+    #[diesel(table_name = crate::schema::users)]
+    #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
+    #[serde(rename_all = "camelCase")]
+    pub struct User {
+        pub id: i32,
+        pub username: String,
+        #[diesel(column_name = firstName)]
+        pub first_name: String,
+        #[diesel(column_name = lastName)]
+        pub last_name: String,
+    }
+
+    #[derive(Queryable, Selectable)]
+    #[diesel(table_name = crate::schema::books)]
+    #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
+    pub struct Book {
+        pub id: i32,
+        pub title: String,
+        pub author: String,
+    }
 }
