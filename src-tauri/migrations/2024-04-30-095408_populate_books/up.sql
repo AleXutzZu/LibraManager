fe8291db172c91104a7d01e9cf48e85f22ab9eb7
AleXutzZu <davidalex_robert@yahoo.com>
2024-04-30 13:11:25 +0300

    Added some book dummy data

---
+++ src-tauri/migrations/2024-04-30-095408_populate_books/up.sql
@@ -0,0 +1,16 @@
+-- Your SQL goes here
+
+INSERT INTO books (isbn, title, author)
+VALUES ('9787940073295', 'To Kill a Mockingbird', 'Harper Lee');
+
+INSERT INTO books (isbn, title, author)
+VALUES ('9784802791090', '1984', 'George Orwell');
+
+INSERT INTO books (isbn, title, author)
+VALUES ('9785442546439', 'Pride and Prejudice', 'Jane Austeen');
+
+INSERT INTO books (isbn, title, author)
+VALUES ('9783221491642', 'The Great Gatsby', 'F. Scott Fitzgerald');
+
+INSERT INTO books (isbn, title, author)
+VALUES ('9789947055908', 'Moby-Dick', 'Herman Melville');
