677bc14f24729f70781a5eef325a3dd86413da9c
AleXutzZu <alex.david@eminescusm.ro>
2024-05-10 08:56:11 +0300

    Fixed the validation schema and added error handling

--- src/components/settings/AccountSettings.tsx
+++ src/components/settings/AccountSettings.tsx
@@ -1,5 +1,5 @@
 import {authProvider, User} from "../../auth/auth.ts";
-import {ActionFunctionArgs, redirect, useNavigate, useSubmit} from "react-router-dom";
+import {ActionFunctionArgs, redirect, useActionData, useNavigate, useSubmit} from "react-router-dom";
 import * as Yup from "yup";
 import {Form, Formik} from "formik";
 import Input from "../util/Input.tsx";
@@ -7,7 +7,7 @@ import {invoke} from "@tauri-apps/api/tauri";
 import {useRootData} from "../util/useRootData.ts";
 
 export async function loader() {
-    if (!authProvider.isAuthenticated) return redirect("/login");
+    if (!authProvider.isAuthenticated()) return redirect("/login");
     return null;
 }
 
@@ -27,24 +27,31 @@ export async function action({request}: ActionFunctionArgs) {
     try {
         await invoke("update_user", {user, password});
     } catch (error) {
-        console.log("Actual error", error);
+        return {
+            error: "Datele nu sunt corecte"
+        }
     }
 
     return redirect("/");
 }
 
 export default function AccountSettings() {
-    const {user} = useRootData() as {user: User};
+    const {user} = useRootData() as { user: User };
     const submit = useSubmit();
     const navigate = useNavigate();
+    const error = useActionData() as { error: string } | undefined;
 
     const validationSchema = Yup.object({
         firstName: Yup.string().required("Prenumele este obligatoriu").matches(/^\w+$/, {message: "Prenumele este invalid"}),
         lastName: Yup.string().required("Numele este obligatoriu").matches(/^\w+$/, {message: "Numele este invalid"}),
         newPassword: Yup.string().matches(/^\w+$/, {message: "Parola este invalidă"})
-            .min(8, "Parola trebuie să aibă minim 8 caractere")
-            .max(20, "Parola poate avea maxim 20 de caractere"),
-        repeatPassword: Yup.string().oneOf([Yup.ref("newPassword")], "Parolele nu se potrivesc"),
+            .min(8, "Nu folosești min. 8 caractere")
+            .max(16, "Folosești peste 16 caractere"),
+        repeatPassword: Yup.string().oneOf([Yup.ref("newPassword")], "Parolele nu se potrivesc").when('newPassword', {
+            is: (value: string | undefined) => value && value.length != 0,
+            then: schema => schema.defined("Parola nu poate lipsi"),
+            otherwise: schema => schema.optional(),
+        }),
         password: Yup.string().required("Parola este obligatorie").matches(/^\w+$/, {message: "Parola este invalidă"}),
     })
 
@@ -52,7 +59,8 @@ export default function AccountSettings() {
         <div className="flex flex-grow">
             <div className="m-auto shadow-black-10 shadow-md rounded-2xl bg-black-5">
                 <div className="py-8 px-4 mx-auto lg:py-16">
-                    <h2 className="mb-4 text-2xl font-bold">Editează informații</h2>
+                    {!error && <h2 className="mb-4 text-2xl font-bold">Editează informații</h2>}
+                    {error && <h2 className="mb-4 text-2xl font-bold text-red">{error.error}</h2>}
                     <Formik initialValues={{
                         firstName: user.firstName,
                         lastName: user.lastName,
@@ -63,7 +71,7 @@ export default function AccountSettings() {
                         submit(values, {method: "post"});
                     }}
                             validationSchema={validationSchema}>
-                        {formik => (
+                        {_formik => (
                             <Form>
                                 <div className="grid grid-cols-2 gap-6">
                                     <div className="w-full">
@@ -95,7 +103,7 @@ export default function AccountSettings() {
                                     </div>
 
                                     <div className="w-full">
-                                        <button type="submit" disabled={formik.isSubmitting}
+                                        <button type="submit"
                                                 className="w-full block items-center px-0.5 py-1.5 mt-6 text-black-5 text-lg font-medium text-center bg-green rounded-2xl">
                                             Salvare
                                         </button>
