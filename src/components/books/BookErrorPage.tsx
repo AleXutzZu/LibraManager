ed1e7b446df3d5ef505f95122db74863016ca447
AleXutzZu <davidalex_robert@yahoo.com>
2024-05-11 17:23:52 +0300

    Added error pages for client and book routes

--- src/components/books/BookErrorPage.tsx
+++ src/components/books/BookErrorPage.tsx
@@ -1,4 +1,12 @@
+import {Link} from "react-router-dom";
+
 export default function BookErrorPage() {
 
-    return <></>
+    return <div className="flex-grow flex">
+        <div
+            className="m-auto bg-black-5 rounded-xl shadow-black-10 border flex flex-col items-center justify-evenly px-4 py-4 h-44">
+            <h1 className="font-bold text-xl">Cartea nu a putut fi găsită în baza de date.</h1>
+            <Link to="/books/create" className="font-medium px-2 py-2 bg-orange text-black-5 rounded-2xl">Adaugă o nouă carte</Link>
+        </div>
+    </div>
 }
--- src/components/clients/ClientErrorPage.tsx
+++ src/components/clients/ClientErrorPage.tsx
@@ -1,10 +1,12 @@
-import {useRouteError} from "react-router-dom";
+import {Link} from "react-router-dom";
 
+export default function BookErrorPage() {
 
-export default function ClientErrorPage() {
-    const error = useRouteError();
-    console.log(error);
-    return (
-        <>{error}</>
-    )
+    return <div className="flex-grow flex">
+        <div
+            className="m-auto bg-black-5 rounded-xl shadow-black-10 border flex flex-col items-center justify-evenly px-4 py-4 h-44">
+            <h1 className="font-bold text-xl">Clientul nu a putut fi găsit în baza de date.</h1>
+            <Link to="/clients/create" className="font-medium px-2 py-2 bg-orange text-black-5 rounded-2xl">Adaugă un nou client</Link>
+        </div>
+    </div>
 }
