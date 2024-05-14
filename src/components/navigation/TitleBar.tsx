088da0bf3f740c03089c601bfdbed9445b6cfcee
AleXutzZu <davidalex_robert@yahoo.com>
2024-05-14 22:01:53 +0300

    Fixed image source

--- src/components/navigation/TitleBar.tsx
+++ src/components/navigation/TitleBar.tsx
@@ -21,7 +21,7 @@ export default function TitleBar() {
 
     return (
         <div data-tauri-drag-region={true} className="flex justify-between h-8 bg-black-25 items-center px-2 border-b">
-            <img src="/public/logo.png" alt="Logo" className="h-6 w-auto" data-tauri-drag-region={true}/>
+            <img src="/logo.png" alt="Logo" className="h-6 w-auto" data-tauri-drag-region={true}/>
             <div className="flex space-x-4 items-center justify-center">
                 <div className="cursor-pointer" onClick={() => appWindow.minimize()}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
