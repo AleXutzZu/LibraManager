97ff00f9738f6e7bbbc1d1bc77c9a1e92972b027
AleXutzZu <alex.david@eminescusm.ro>
2024-05-15 08:14:12 +0300

    Removed the redundant sql code and redid the migration

--- src-tauri/migrations/2024-05-06-101758_intersection_entity/up.sql
+++ src-tauri/migrations/2024-05-06-101758_intersection_entity/up.sql
@@ -10,9 +10,3 @@ CREATE TABLE borrows
     FOREIGN KEY (clientID) REFERENCES clients (id) ON DELETE CASCADE,
     FOREIGN KEY (bookISBN) REFERENCES books (isbn) ON DELETE CASCADE
 );
-
-INSERT INTO clients(id, firstName, lastName, email, phone)
-VALUES ('ea25ee6d-adaa-4256-9101-6d1e80dbf221', 'Alex', 'David', 'davidalexrobertyt@gmail.com', '0758061633');
-
-INSERT INTO borrows (id, clientID, bookISBN, startDate, endDate, returned)
-VALUES (1, 'ea25ee6d-adaa-4256-9101-6d1e80dbf221', '9787940073295', '2024-05-07', '2024-05-21', 0);
